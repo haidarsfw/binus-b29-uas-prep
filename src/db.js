3dd49a4c805453339ac042b195af9a722544ce3b
Haidar Shofwan Bani <haidarshofwan@192.168.1.5>
2025-12-23 19:59:39 +0700

    Add Rangkuman Mentor PDF for MIS
    - Added mentorPPT entry with Google Drive link
    - File ID: 1BVqxMm333yl6p2TahXa-Tdu87RVQz1Wq

--- src/db.js
+++ src/db.js
@@ -105,7 +105,9 @@ export const DB = {
                 addendum: [
                     { title: 'Addendum: Detail Teknis Hafalan Slide', contentKey: 'tambahan', type: 'native' },
                 ],
-                mentorPPT: [],
+                mentorPPT: [
+                    { title: 'Rangkuman Mentor MIS', driveId: '1BVqxMm333yl6p2TahXa-Tdu87RVQz1Wq', type: 'pdf' },
+                ],
             },
         },
         intro: {
